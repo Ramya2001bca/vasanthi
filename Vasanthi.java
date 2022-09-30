d09bfe39fa8bb457191990b305cd9b650d54935a
Ramya2001bca <113408822+Ramya2001bca@users.noreply.github.com>
2022-09-30 09:56:15 +0530

    Add files via upload

---
+++ Vasanthi.java
@@ -0,0 +1,14 @@
+class Vasanthi{
+public static void main(String[] args)
+{
+int Rollno=11;
+String Name="Ramya"; 
+char Nameothefirstletter='R';
+short  age=20;
+long phonenumber=1234567890;
+float number1=12.5f;
+double number2=121.999999;
+boolean Result10exam=true;
+System.out.println(Rollno+" \n"+Name+" \n"+Nameothefirstletter+"\n "+age+"\n "+phonenumber+" \n"+number1+" \n"+number2+" \n"+Result10exam);
+}
+}
